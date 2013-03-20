881074b290dcab61f89346944006de151ba31837
Martin Schürrer <martin@schuerrer.org>
2013-03-20 08:04:23 +0100

    Sidekiq doesn't kill via exception?

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -4,9 +4,16 @@ class HardWorker
   include Sidekiq::Worker
   def perform(name, count)
     ActiveRecord::Base.transaction do
-      DBLog.create!(value: "Before sleep")
-      sleep 50
-      DBLog.create!(value: "After sleep")
+      begin
+        DBLog.create!(value: "Before sleep")
+        sleep 50
+        DBLog.create!(value: "After sleep")
+      rescue Exception => e
+        Sidekiq.logger.warn "Exception caught"
+        Sidekiq.logger.warn e
+      ensure
+        Sidekiq.logger.warn "inner ensure"
+      end
     end
   ensure
     DBLog.create!(value: "Exiting")
